0733f72f79e99b88394b48bead2f1d390b686de5
Marc Esquerra <esquerra@gmail.com>
2021-01-16 14:41:28 +0000

    Add the concept of 'global-docs'

--- src/main/scala/com/bryghts/r2bot/Capability.scala
+++ src/main/scala/com/bryghts/r2bot/Capability.scala
@@ -1,4 +1,9 @@
 package com.bryghts.r2bot
 
+import sbt._
+
 trait Capability {
+
+  def applyConfiguration(p: Project): Project
+
 }
--- src/main/scala/com/bryghts/r2bot/R2botPlugin.scala
+++ src/main/scala/com/bryghts/r2bot/R2botPlugin.scala
@@ -4,24 +4,28 @@ import sbt._
 import sbt.Keys._
 import sbt.plugins.JvmPlugin
 
-object R2botPlugin extends AutoPlugin {
+object R2botPlugin extends AutoPlugin with R2ExtensionMethods {
+
+  lazy val r2GlobalDocs =
+    project
+      .in(file("target/hidden/globalDocs"))
+      .r2Docs
 
   override def trigger = allRequirements
   override def requires = JvmPlugin
 
   object autoImport extends R2ExtensionMethods {
-    val r2GenDocs = taskKey[Unit]("Generates docs, including '*._no_ext_' ones")
+    val r2GenGlobalDocs = taskKey[Unit]("Generates the global, not associated to any particular project, including '*._no_ext_' ones")
+    val r2GlobalDocsVariables = settingKey[Map[String, String]]("")
     val r2 = com.bryghts.r2bot.r2
   }
 
-  import autoImport._
-
-  override lazy val projectSettings = Seq(
-  )
-
+  override lazy val projectSettings = Seq()
   override lazy val buildSettings = Seq()
-
-  override lazy val globalSettings = Seq()
+  override lazy val globalSettings =
+    r2.globalSettings(r2GlobalDocs)
+  override lazy val extraProjects: Seq[Project] =
+    Seq(r2GlobalDocs)
 }
 
 object r2 {
@@ -31,9 +35,9 @@ object r2 {
   import com.bryghts.r2bot.PathOps._
 
   def globalSettings(docs: ProjectReference): Seq[Setting[_]] = Seq (
-    r2GenDocs := {
+    r2GenGlobalDocs := {
       val _ = (mdoc.in(docs)).toTask("").value
-      val docsRoot = path("docs")
+      val docsRoot = path("global-docs")
       val extension = "._no_ext_"
       docsRoot
         .allFilesRecursively
@@ -61,26 +65,21 @@ trait R2ExtensionMethods {
 
   implicit class R2ProjectOps(val p: Project) {
 
-    def r2Docs(root: => Project, dependOnRoot: Boolean = true): Project = {
-      lazy val r : ClasspathDep[ProjectReference] = root
-      p.in(file("documentation"))
-       .enablePlugins(MdocPlugin)
-       .dependsOn((if (dependOnRoot) List(r) else List.empty) :_*)
-       .settings(
-         if (dependOnRoot) Seq(scalaVersion := (scalaVersion.in(root)).value) else Seq()
-        )
+    def r2Docs(): Project = {
+      p.enablePlugins(MdocPlugin)
        .settings(
           skip in publish := true,
+          mdocIn :=  (ThisBuild / baseDirectory).value / "global-docs",
           mdocOut := (ThisBuild / baseDirectory).value,
           mdocExtraArguments ++= Seq(
             "--markdown-extensions", "md",
             "--markdown-extensions", "_no_ext_"
           ),
 
-          mdocVariables ++= Map(
+          mdocVariables ++= r2GlobalDocsVariables.value ++ Map(
             "VERSION" -> version.value,
-            "NAME" -> (name.in(root)).value,
-            "GROUP" -> (organization.in(root)).value,
+            // "NAME" -> (name.in(root)).value,
+            // "GROUP" -> (organization.in(root)).value,
             "YEAR" -> {
               val initialYear = "2020"
               val currentYear = java.time.Year.now.getValue().toString
@@ -88,7 +87,7 @@ trait R2ExtensionMethods {
               if (initialYear == currentYear) initialYear
               else s"$initialYear-$currentYear"
             },
-            "COPYRIGHT_HOLDER" -> "Marc Esquerra <esquerra@bryghts.com>"
+            // "COPYRIGHT_HOLDER" -> "Marc Esquerra <esquerra@bryghts.com>"
           )
 
         )
@@ -104,7 +103,7 @@ trait R2ExtensionMethods {
            runClean,                               // : ReleaseStep
            runTest,                                // : ReleaseStep
            setReleaseVersion,                      // : ReleaseStep
-           ReleasePlugin.autoImport.releaseStepTask(r2GenDocs),
+           ReleasePlugin.autoImport.releaseStepTask(r2GenGlobalDocs),
            ReleasePlugin.autoImport.releaseStepCommand("git add ."),
            commitReleaseVersion,                   // : ReleaseStep, performs the initial git checks
            tagRelease,                             // : ReleaseStep
